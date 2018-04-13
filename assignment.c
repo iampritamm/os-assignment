6afe3dec1c69680deaa2da784c4ed616296627e4
Pritam  Maity <38342756+iampritamm@users.noreply.github.com>
2018-04-13 22:56:15 +0530

    Update assignment.c
    allocation of pid

--- assignment.c
+++ assignment.c
@@ -20,20 +20,40 @@ int allocate_map(void)
       return 1;
   }
 } 
+int  allocate_pid()
+{
 void  *threadcall(void *p)
 {
-  while(thread<100)
-  {
   pthread_mutex_lock(&mutex);
   if(thread>=100)
+    int j=1;
+  while(j>=0)
   {
-    pthread_mutux_unlock(&mutuex);
-    break;
-  }
-    thread++;
+    if(pidArr[j].bitmap==0)
+    { 
     pthread_mutex_unlock(&mutex);
+    break;
     }
+      thread++;
+  pthread_mutex_unlock(&mutex); 
+    if(j<threadVar)
+    {
+      pidArr[threadVar].pid=pidArr[j].pid;
+      pid[j].bitmap=1;
+      pidArr[threadVar].bitmap=1;
+      return 0;
+    }
+    else
+    {
+      pidArr[j].pid=tid;
+      tid+=1;
+      pidArr[j].bitmap=1;
+      return tid;
+    }
+  }
+  j++
 }
+  
 void release_pid(int pid)
 {
   int i=0;
@@ -45,6 +65,22 @@ void release_pid(int pid)
       pidArr[i].bitmap=0;
     }
   }
+}
+void* threadCall(void* voidA)
+{
+  pthread_mutex_lock(&mutex);
+  allocate_pid();
+  sleep(1);
+  threadVar++;
+  printf("\n\nProcess Number: %d",threadVar);
+  printf("\n\nProcess Id Allocated:%d \n",pid[threadVar-1].pid);
+  if(threadVar==31)
+  {
+    release_pid(320);
+    release_pid(321);
+    release_pid(322);
+  }
+  pthread_mutex_unlock(&mutex);
 }
 int main()
 {
